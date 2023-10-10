d141df1e41d2f01195a23b08e33814abff06cf51
serper35 <serper.wolf@gmail.com>
2023-10-10 21:12:47 +0300

    solved

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car extends Transport implements Engine {
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
---
+++ src/StaffRemarks.java
@@ -0,0 +1,21 @@
+public class StaffRemarks {
+
+    private String staff;
+    public  void almostDone() {
+        System.out.println("------------------\nПочти готово, хозяин.\n------------------");
+    }
+
+    public String letsGo() {
+
+        return "Ну, понеслась! ";
+    }
+
+    public String willBeGood() {
+
+        return " будет лучше новой!\n------------------";
+    }
+
+    public void done() {
+        System.out.println("------------------\nПринимай работу, хозяин!");
+    }
+}
